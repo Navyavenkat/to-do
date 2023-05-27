1104748f5666dc92c7d45457f89471fd9f5ca79d
navyavenkat <navyavenkat03@gmail.com>
2023-05-26 23:25:43 -0700

    Update MVC to update,delete method

--- controllers/to_do.js
+++ controllers/to_do.js
@@ -5,5 +5,17 @@ const createANewToDoItem=(req,res)=>{
     // res.send('new list')
     res.status(200).json(req.body)
 }
+const getAToDoItem=(req,res)=>{
+    res.send(`Displaying item with id ${req.params.id}`)
+    //res.status(200).json(req.body)
+}
 
-module.exports={getAllToDoItems,createANewToDoItem}
+const updateAToDoItem=(req,res)=>{
+res.send(`Update item with id ${req.params.id}`)
+}
+
+const deleteAToDoItem=(req,res)=>{
+    res.send(`Deleting item with id ${req.params.id}`)
+    }
+
+module.exports={getAllToDoItems,createANewToDoItem,getAToDoItem}
--- routes/to_do.js
+++ routes/to_do.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const router=express.Router()
-const {getAllToDoItems,createANewToDoItem}=require('../controllers/to_do')
+const {getAllToDoItems,createANewToDoItem,getAToDoItem}=require('../controllers/to_do')
 router.route('/').get(getAllToDoItems).post(createANewToDoItem)
+router.route('/:id').get(getAToDoItem)
 // router.get("/",(req,res)=>{
 //     res.send("List of all to-do-items")
 // })
