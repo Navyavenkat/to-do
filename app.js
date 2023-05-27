c7ebb6036c30e659c23f1729d4a22cb192409d5d
navyavenkat <navyavenkat03@gmail.com>
2023-05-26 22:14:45 -0700

    REST is created and used in router

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express=require('express')
+const  to_do  = require('./to_do')
 const app=express()
 const PORT=3500
 app.get('/',(req,res)=>{
     res.send('working')
 })
+
+app.use('/api/v1/to_do',to_do)
 app.listen(PORT,console.log(`Server runs at http://localhost:${PORT}`))
